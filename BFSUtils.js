d6122576066f7e0c1c8052defcd4aedf131c1d27
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-01-10 15:48:25 -0800

    csv formatted data saving and parsing functions, some other fixes

--- BFSUtils.js
+++ BFSUtils.js
@@ -120,6 +120,22 @@ export const getFileSize = (filename,dir='/data',onread=(size)=>{console.log(siz
     });
 }
 
+export const writeFile = (filename, data, dir='/data', onwrite=(data)=>{}) => {
+    fs.writeFile(dir+'/'+filename, data, (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+        onwrite(data);
+    });
+}
+
+export const appendFile = (filename, data, dir='/data', onwrite=(data)=>{}) => {
+    fs.appendFile(dir+'/'+filename, data, (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+        onwrite(data);
+    });
+}
+
 export const deleteFile = (path="/data/" + 'sessionName', ondelete=listFiles) => {
     if (path != ''){
         fs.unlink(path, (e) => {
@@ -131,12 +147,32 @@ export const deleteFile = (path="/data/" + 'sessionName', ondelete=listFiles) =>
     }
 }
 
-export const parseDBData = (data,head,filename,hasend=true,parser=(lines,head)=>{}) => {
+
+export const parseCSVData = (
+    data,
+    head,
+    filename,
+    hasend=true,
+    parser=(lines,head,filename) => { //generic parser for CSV files
+        let result = {filename:filename,head:head};
+        let headers = head.split(',');
+        for(let i = 0; i < lines.length; i++){
+            let line = lines[i].split(',');
+            for(let j = 0; j < line.length; j++){
+                if(!result[headers[j]]) result[headers[j]]
+                result[headers[j]] = line[j];
+            }
+        }
+    
+        return result; 
+    
+    }
+) => {
     let lines = data.split('\n'); 
     lines.shift(); 
 
     if(hasend === false) lines.pop(); //pop first and last rows if they are likely incomplete
-    let result = parser(lines,head);
+    let result = parser(lines,head,filename);
     return result;
 }
 
@@ -206,8 +242,119 @@ export const listFiles = (dir='/data', onload=(directory)=>{},fs_html_id=undefin
     });
 }
 
-//Write CSV data in chunks to not overwhelm memory
-export const writeToCSV = async (filename='sessionName',dir='/data') => {
+//pass an object with settings and data to process into CSV format
+/**
+ * option format:
+ * {
+ *  filename: 'filename',
+ *  save: true, //will write a local CSV
+ *  write:true, //will write to indexedDB, appending a file if it already exists
+*   header: ['timestamp','UTC','FP1','etc'],
+ *  data: [{ //order of data is order of columns 
+ *      x: [], //e.g. utc times. Used to create rows of data with the first column set to this
+ *      timestamp: [], //e.g. ISO timestamps
+ *      FP1; [1,2,3,4,5] //e.g.  filtered or raw data
+ *    },
+ *    {
+ *      x: [], //e.g. utc times. Will splice rows with different timings into the same CSV
+ *      timestamp: [], //e.g. ISO timestamps     
+ *      FP1_FFT: [[0,1,2,3],[...],...], //e.g. periodic FFT data with different timing. Arrays of arrays get multiple columns
+ *      FP2:[etc],...   
+ *    }]
+ *  }
+ * }
+ * 
+ * result: [
+ *  'timestamp,UTC,FP1,FP1_FFT,...,\n',
+ *  '2012-12-12T12:12:12.000Z,123456,12345,123,...,\n',
+ * ]
+ * 
+ */
+//spits out an array of CSV rows in string format with endlines added
+export const processDataForCSV = (options={}) => {
+    let header = '';
+    let headeridx = 0;
+    let lines = {}; //associative array (e.g. timestamp = row)
+    options.data.forEach((obj, i) => {
+        if(Array.isArray(obj)) { //unstructured data just gets pushed into a column
+            for(let j = 0; j < obj.length; j++) {
+                if(!lines[j]) lines[j] = '';
+                
+                if(Array.isArray(obj[j])) {
+                    if(j === 0) header[headeridx] += options.header[headeridx]+new Array(obj[j].length-1).fill('').join(','); //add empty spaces
+                    lines[j] += obj[j].join(',') + ',';
+                    headeridx+=obj[j].length;
+                }
+                else {
+                    if(j === 0) header[headeridx] += options.header[headeridx]+',';
+                    lines[j] += obj[j] + ',';
+                    headeridx++;
+                }
+            }
+        } else if (typeof obj === 'object'){
+            let x;
+            if(obj.x) {
+               x = obj.x; 
+            } else if (obj.timestamp) {
+                x = obj.timestamp;
+            }
+            for(let j = 0; j < x.length; j++) {
+                if(j === 0) header[headeridx]
+                if(!lines[x[j]]) lines[x[j]] = x[j] + ',';
+                if(obj.timestamp) {
+                    lines[x[j]] += toISOLocal(obj.timestamp[j]) + ',';
+                }
+
+                for(const prop in obj) {
+                    if(prop !== 'x') {
+                        if(!lines[x[j]]) lines[x[j]] = '';
+                
+                        if(Array.isArray(obj[prop][j])) {
+                            if(j === 0) header[headeridx] += options.header[headeridx]+Array(obj[prop][j].length-1).fill('').join(','); //add empty spaces
+                            lines[x[j]] += obj[prop][j].join(',') + ',';
+                            headeridx+=obj[prop][j].length;
+                        }
+                        else {
+                            if(j === 0) header[headeridx] += options.header[headeridx]+',';
+                            lines[x[j]] += obj[prop][j] + ',';
+                            headeridx++;
+                        }
+                    }
+                }
+            }  
+        }
+    });
+
+    header.splice(header.length-1,1); //remove last comma
+    header += '\n';
+
+    let joined = '';
+
+    for(const prop in lines) {
+        lines[prop].splice(lines[prop].length-1,1); //splice off the trailing comma
+        joined +=  lines[prop] + '\n'; //add a newline
+    }
+
+    let result = {filename:options.filename,header:header,body:joined};
+
+    if(options.save) {
+        CSV.saveCSV(header+joined,options.filename);
+    }
+
+    if(options.write) {
+        fs.exists(options.filename, (exists) => {
+            if(exists) 
+                appendFile(options.filename, joined);
+            else
+                appendFile(options.filename, header+joined);
+        });
+    }
+
+    return result;
+}
+
+//Write IndexedDB data into a CSV, in chunks to not overwhelm memory. This is for pre-processed data
+export const writeToCSV = async (filename='sessionName',dir='/data',fileSizeLimitMb=10) => {
     if (filename != ''){
         fs.stat(dir + '/' + filename, (e, stats) => {
             if (e) throw e;
@@ -216,7 +363,7 @@ export const writeToCSV = async (filename='sessionName',dir='/data') => {
             fs.open(dir + '/' + filename, 'r', (e, fd) => {
                 if (e) throw e;
                 let i = 0;
-                let maxFileSize = this.state.data.fileSizeLimitMb * 1024 * 1024;
+                let maxFileSize = fileSizeLimitMb * 1024 * 1024;
                 let end = maxFileSize;
                 if (filesize < maxFileSize) {
                     end = filesize;
@@ -252,6 +399,7 @@ export const writeToCSV = async (filename='sessionName',dir='/data') => {
         console.error('File name is not defined.')
     }
 }
+
 
 export function toISOLocal(d) { //pass in a new Date(utc timestamp) object
     var z  = n =>  ('0' + n).slice(-2);
